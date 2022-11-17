d55954ec0b8e280bf650067a7bba487976f8d470
LeloM101 <lelomalinga101@gmail.com>
2022-11-16 19:50:24 -0800

    tasks

--- environment.c
+++ environment.c
@@ -22,13 +22,12 @@ char *_getenv(info_t *info, const char *name)
 {
 list_t *node = info->env;
 char *p;
-
 while (node)
 {
-  p = starts_with(node->str, name);
-  if (p && *p)
-    return (p);
-  node = node->next;
+p = starts_with(node->str, name);
+if (p && *p)
+return (p);
+node = node->next;
 }
 return (NULL);
 }
@@ -43,11 +42,11 @@ int _mysetenv(info_t *info)
 {
 if (info->argc != 3)
 {
-  _eputs("Incorrect number of arguements\n");
-  return (1);
+_eputs("Incorrect number of arguements\n");
+return (1);
 }
 if (_setenv(info, info->argv[1], info->argv[2]))
-  return (0);
+return (0);
 return (1);
 }
 
@@ -55,7 +54,7 @@ return (1);
 * _myunsetenv - Remove an environment variable
 * @info: Structure containing potential arguments. Used to maintain
 *        constant function prototype.
-*  Return: Always 0
+* Return: Always 0
 */
 int _myunsetenv(info_t *info)
 {
@@ -63,19 +62,18 @@ int i;
 
 if (info->argc == 1)
 {
-  _eputs("Too few arguements.\n");
-  return (1);
+_eputs("Too few arguements.\n");
+return (1);
 }
 for (i = 1; i <= info->argc; i++)
-  _unsetenv(info, info->argv[i]);
-
+_unsetenv(info, info->argv[i]);
 return (0);
 }
 
 /**
 * populate_env_list - populates env linked list
 * @info: Structure containing potential arguments. Used to maintain
-*          constant function prototype.
+* constant function prototype.
 * Return: Always 0
 */
 int populate_env_list(info_t *info)
@@ -84,7 +82,7 @@ list_t *node = NULL;
 size_t i;
 
 for (i = 0; environ[i]; i++)
-  add_node_end(&node, environ[i], 0);
+add_node_end(&node, environ[i], 0);
 info->env = node;
 return (0);
 }
